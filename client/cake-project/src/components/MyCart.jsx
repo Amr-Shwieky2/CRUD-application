974d75757a9f78c3e55470f95ba0f922983a6946
amalshweiki <amalshweiki88@gmail.com>
2023-11-25 20:08:57 +0200

    order place amal  commit

--- client/cake-project/src/components/MyCart.jsx
+++ client/cake-project/src/components/MyCart.jsx
@@ -1,9 +1,44 @@
 import { useCart } from "../context/CartContext";
 import { TbCurrencyShekel } from "react-icons/tb";
-
+import { useState } from "react";
 import "../style/cart.css";
 const MyCart = () => {
   const { cart } = useCart();
+  const [customerDetails, setcustomerDetails] = useState({
+    name: "",
+    address: "",
+  });
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const handelInputChange = (e) => {
+    setcustomerDetails({
+      ...customerDetails,
+      [e.target.name]: e.target.name,
+      [e.target.adress]: e.target.address,
+    });
+  };
+  const calculateTotalPrice = () => {
+    const total = cart.reduce((acc, cake) => acc + cake.price, 0);
+    setTotalPrice(total);
+  };
+
+  const handlePlaceOrder = (e) => {
+    e.preventDefault();
+    const productIds = cart.map((pro) => pro._id);
+    const orderData = {
+      products: productIds,
+      totalPrice: calculateTotalPrice(cart),
+      customerDetails,
+    };
+    //fetch
+    // fetch("http://", {
+    //   method: "PPST",
+    //   Headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(orderData),
+    // }).then((response) => response.json());
+  };
   return (
     <div>
       <div className="cart">
@@ -18,24 +53,26 @@ const MyCart = () => {
             ))}
           </form>
 
-          <form className="placeOrder-form">
+          <form onSubmit={handlePlaceOrder} className="placeOrder-form">
             <input
               className="input-style"
               type="text"
-              value=""
-              onChange=""
+              name="name"
+              value={customerDetails.name}
+              onChange={handelInputChange}
               placeholder="Name"
             ></input>
             <input
               className="input-style"
               type="text"
-              value=""
-              onChange=""
-              placeholder="address"
+              name="address"
+              value={customerDetails.address}
+              onChange={handelInputChange}
+              placeholder="Address"
             ></input>
             <button
               className="btn  mx-center me-2 btn-placeorder d-flex justify-content-center"
-              onClick=""
+              type="submit"
             >
               Place Order
             </button>

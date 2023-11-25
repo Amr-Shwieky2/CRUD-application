8021ad168b4dd79aac05ccd517811d9a1eb2a415
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-11-25 19:20:42 +0200

    CRUD

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -1,18 +1,40 @@
-
-
 const orderController = {
-    createOrder: async (req, res) => {
-        try {
-        console.log(req.body);
-        const cake = await cakes.create(req.body);
-        res.status(201).json({
-            success: true,
-            data: cake,
-        });
-        } catch (error) {
-        res.status(400).json({ success: false });
-        }
+  getAllOrders: async (req, res) => {
+    try {
+      const order = await cakes.find();
+      res.status(201).json({
+        success: true,
+        data: order,
+      });
+    } catch (error) {
+      res.status(400).json({ success: false });
     }
-}
+  },
+
+  createOrder: async (req, res) => {
+    try {
+      console.log(req.body);
+      const order = await cakes.create(req.body);
+      res.status(201).json({
+        success: true,
+        data: order,
+      });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  },
+
+  getOrderById: async (req, res) => {
+    try {
+      const order = await cakes.findById(req.params.id);
+      res.status(201).json({
+        success: true,
+        data: order,
+      });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  },
+};
 
 module.exports = orderController;
